52b2fc97555f15d84c2fe75b76a3b8460e52eef3
Massimo Centemeri <massimo.centemeri@evolutionpeople.it>
2022-01-20 21:27:41 +0100

    fix scalapay integration test 6

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -462,6 +462,7 @@ const placeOrder = order => {
 const scalapayBackendBaseURL = 'https://scalapay.fas-rocca.agenziadigital.it/api/v1'
 
 const scalapayOrderInit = orderData => {
+  console.log(orderData)
   return apiClient
     .post(scalapayBackendBaseURL + '/order', orderData)
     .then(response => {
